7534e926709d07417b08a6a204bc02b67e0850ed
FailedShack <failedshack@gmail.com>
2019-06-16 04:35:15 +0200

    Add patch to remove annoying system update warning

---
+++ USBHelperInjector/Patches/SystemUpdateWarningPatch.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace USBHelperInjector.Patches
+{
+    [HarmonyPatch]
+    class SystemUpdateWarningPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.DeclaredProperty(ReflectionHelper.Settings, "Show552Warning").GetGetMethod(true);
+        }
+
+        static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
