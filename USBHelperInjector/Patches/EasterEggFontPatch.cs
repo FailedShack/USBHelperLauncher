b90d5f35fe85a6ddcbd9133bc50abf23fa00e9a8
Simon J <me@shiftinv.cc>
2019-07-14 20:25:48 +0200

    Add easter egg for April 1st

---
+++ USBHelperInjector/Patches/EasterEggFontPatch.cs
@@ -0,0 +1,42 @@
+﻿using Harmony;
+using System;
+using System.Drawing;
+using System.Reflection;
+using USBHelperInjector.Patches.Attributes;
+
+namespace USBHelperInjector.Patches
+{
+    [Optional]
+    [HarmonyPatch]
+    class EasterEggFontPatch
+    {
+        private static string fontName;
+
+        static EasterEggFontPatch()
+        {
+            if (DateTime.Now.Month == 4 && DateTime.Now.Day == 1)
+            {
+                // check if the font exists on the system
+                var name = "Comic Sans MS";
+                var font = new Font(name, 12);
+                if (font.Name == name)
+                {
+                    fontName = name;
+                }
+            }
+        }
+
+        static MethodBase TargetMethod()
+        {
+            return typeof(FontFamily).GetMethod("CreateFontFamily", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        static void Prefix(ref object __0)
+        {
+            if (fontName != null)
+            {
+                __0 = fontName;
+            }
+        }
+    }
+}
