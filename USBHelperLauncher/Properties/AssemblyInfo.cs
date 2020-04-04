da103806a8766e27b34af2adac1445c1191544ac
FailedShack <failedshack@gmail.com>
2020-04-05 00:38:50 +0200

    Increment version number

--- USBHelperLauncher/Properties/AssemblyInfo.cs
+++ USBHelperLauncher/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.17.0.1")]
-[assembly: AssemblyFileVersion("0.17.0.1")]
+[assembly: AssemblyVersion("0.17.0.2")]
+[assembly: AssemblyFileVersion("0.17.0.2")]
