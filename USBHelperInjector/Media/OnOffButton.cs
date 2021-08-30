27661b71e6584176df43dbbe775dac82829d7764
FailedShack <failedshack@gmail.com>
2021-08-30 23:45:04 +0200

    Invert OnOffButton colors when background is too dark

--- USBHelperInjector/Media/OnOffButton.cs
+++ USBHelperInjector/Media/OnOffButton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
@@ -8,6 +9,8 @@ namespace USBHelperInjector.Media
     class OnOffButton : Button
     {
         public event EventHandler StateChanged;
+
+        private Bitmap bitmap;
 
         private bool _state;
         public bool State
@@ -20,7 +23,6 @@ namespace USBHelperInjector.Media
             {
                 _state = value;
                 Image = value ? OnImage : OffImage;
-                Size = Image.Size;
                 StateChanged?.Invoke(this, null);
             }
         }
@@ -29,10 +31,16 @@ namespace USBHelperInjector.Media
         public Image OnImage { get; set; }
         public Image OffImage { get; set; }
 
+        public static Brush AlmostBlack = new SolidBrush(Color.FromArgb(25, 25, 25));
+        public static Brush AlmostWhite = new SolidBrush(Color.FromArgb(230, 230, 230));
+
         protected override void OnCreateControl()
         {
+            Debug.Assert(OnImage.Size == OffImage.Size);
+            Size = OnImage.Size;
             StateChanged?.Invoke(this, null);
             Image = _state ? OnImage : OffImage;
+            bitmap = new Bitmap(ClientSize.Width, ClientSize.Height);
             base.OnCreateControl();
         }
 
@@ -56,12 +64,39 @@ namespace USBHelperInjector.Media
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            var g = e.Graphics;
-            var rect = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height);
-            ButtonRenderer.DrawParentBackground(g, rect, this);
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            if (Hover) g.FillEllipse(Brushes.White, rect);
-            g.DrawImage(Image, rect);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                var rect = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height);
+                ButtonRenderer.DrawParentBackground(g, rect, this);
+                var dark = bitmap.GetPixel(0, 0).GetBrightness() < 0.1;
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                if (Hover) g.FillEllipse(dark ? AlmostBlack : AlmostWhite, rect);
+                g.DrawImage(dark ? Invert(Image) : Image, rect);
+                e.Graphics.DrawImageUnscaled(bitmap, 0, 0);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            bitmap.Dispose();
+            AlmostBlack.Dispose();
+            AlmostWhite.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private Image Invert(Image image)
+        {
+            var copy = new Bitmap(image);
+            for (var i = 0; i < copy.Height; ++i)
+            {
+                for (var j = 0; j < copy.Width; ++j)
+                {
+                    var inv = copy.GetPixel(i, j);
+                    inv = Color.FromArgb(inv.A, 255 - inv.R, 255 - inv.G, 255 - inv.B);
+                    copy.SetPixel(i, j, inv);
+                }
+            }
+            return copy;
         }
     }
 }
