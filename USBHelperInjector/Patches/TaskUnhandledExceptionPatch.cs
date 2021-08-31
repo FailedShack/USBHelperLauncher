8d7b843fc592a00b5da9ef24458f1c32a89c5c66
FailedShack <failedshack@gmail.com>
2021-08-31 15:22:50 +0200

    Add patch to ensure Tasks cannot fail silently

---
+++ USBHelperInjector/Patches/TaskUnhandledExceptionPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace USBHelperInjector.Patches
+{
+    // Raises exceptions for failed Tasks that have not been awaited for
+    [HarmonyPatch]
+    class TaskUnhandledExceptionPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.FirstMethod(typeof(Task), method => method.Name == "ExecuteWithThreadLocal");
+        }
+
+        static void Postfix(Task __instance)
+        {
+            if (__instance.IsFaulted)
+            {
+                var ex = __instance.Exception;
+                ExceptionDispatchInfo.Capture(ex.InnerException ?? ex).Throw();
+            }
+        }
+    }
+}
