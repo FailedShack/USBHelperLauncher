61c8d889b097ada3969a714828c0d50ce11b9a3e
FailedShack <failedshack@gmail.com>
2023-03-27 00:24:11 +0200

    Add patch to disable borked Nvidia driver version check

---
+++ USBHelperInjector/Patches/NvidiaDriverCheckPatch.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace USBHelperInjector.Patches
+{
+    [HarmonyPatch]
+    class NvidiaDriverCheckPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            return ReflectionHelper.MainModule.GetType("NusHelper.Emulators.Cemu")
+                .GetMethod("CheckNvidiaDriverVersion", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
